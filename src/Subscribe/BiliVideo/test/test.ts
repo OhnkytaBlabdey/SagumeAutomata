27f306290d075d00fe587f336ceb733db0a74a4d
OhnkytaBlabdey <1263189143@qq.com>
2021-06-08 11:03:36 +0800

    移除视频订阅

--- src/Subscribe/BiliVideo/test/test.ts
+++ src/Subscribe/BiliVideo/test/test.ts
@@ -1,9 +1,18 @@
 import video from "../";
 import log from "../../../Logger";
 async function main() {
+    await (await video).removeSubByName(123456, "红科搬");
+    await (await video).addSub(123456, 1311124, "红科搬");
+    await (await video).addSub(123456, 1311124, "红科搬");
+
+    // await (await video).removeSubByName(123456, "红科搬");
     // await (await video).addSub(123456, 1311124, "红科搬");
-    const latestVideo = await (await video).getLatestVideo(1311124);
-    log.info("最新视频", JSON.stringify(latestVideo));
+    // await (await video).removeSubByUid(123456, 1311124);
+
+    // await (await video).removeSubByUid(123456, 114514);
+
+    // const latestVideo = await (await video).getLatestVideo(1311124);
+    // log.info("最新视频", JSON.stringify(latestVideo));
 }
 
 main();

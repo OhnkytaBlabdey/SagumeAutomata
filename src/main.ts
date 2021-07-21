96e45f079ebadcb5d1999b11b7ffa12df906f8aa
OhnkytaBlabdey <1263189143@qq.com>
2021-07-21 17:49:59 +0800

    迷惑

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,14 @@
 import qq from "./QQMessage";
 import live from "./Subscribe/BiliLive";
 import video from "./Subscribe/BiliVideo";
+import dynamic from "./Subscribe/Dynamics";
 
 async function main() {
     qq;
     setTimeout(async () => {
         (await live).run();
         (await video).run();
+        (await dynamic).run();
     }, 1000);
 }
 try {

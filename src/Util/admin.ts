2095d95ea6a8db7effa202a5bad02c7bc88d57d9
OhnkytaBlabdey <1263189143@qq.com>
2021-08-09 21:02:39 +0800

    更新了使用说明

--- src/Util/admin.ts
+++ src/Util/admin.ts
@@ -3,8 +3,8 @@ import { Config } from "../QQMessage/config.interface";
 import config from "../../config/config.json";
 
 const isAdmin = (ev: messageEvent): boolean => {
-    if (ev.sender?.role !== "owner" && ev.sender?.role !== "admin") {
-        return false;
+    if (ev.sender?.role == "owner" || ev.sender?.role == "admin") {
+        return true;
     }
     if ((config as Config)?.su == ev.user_id) {
         return true;

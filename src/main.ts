ebea434cc7f57b9b0b66b0634dd1e74a6b7d7a3e
newive <738767136@qq.com>
2022-03-14 14:45:25 +0800

    -

--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,6 @@ import qq from "./QQMessage";
 import dbHandler from "./DBManager";
 import pluginLoader from "./PluginLoader";
 import qqCommand from "./QQCommand";
-import juejinDaily from "./Plugins/JuejinDaily";
 
 async function main() {
     // wsc连接与数据库初始化为其他服务的前驱
@@ -11,13 +10,7 @@ async function main() {
     await qqCommand.loadCommand();
     await qq.wscConnect();
 }
-
-async function test() {
-    await dbHandler.init();
-    await juejinDaily.getLatestInfo();
-}
-
-test()
+main()
     .then(() => {
         console.log("初始化完成");
     }).catch((e) => {

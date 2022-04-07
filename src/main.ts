93e00faa56f63e1bf99418da07f56d48b0b6a906
newive <738767136@qq.com>
2022-04-07 15:05:17 +0800

    添加新config template

--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,6 @@ import qq from "./QQMessage";
 import dbHandler from "./DBManager";
 import pluginLoader from "./PluginLoader";
 import qqCommand from "./QQCommand";
-import random from "./Plugins/Random";
 
 async function main() {
     // wsc连接与数据库初始化为其他服务的前驱

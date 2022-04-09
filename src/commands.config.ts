10263d21c92d675028f6a997a27989f45ae0a2f9
Ohnkyta <1263189143@qq.com>
2022-04-10 02:10:25 +0800

    fix

--- src/commands.config.ts
+++ src/commands.config.ts
@@ -1,96 +1,96 @@
 export default {
-    "commands": [
+    commands: [
         {
-            "name": "BAIRAdd",
-            "on": true
+            name: "BAIRAdd",
+            on: true,
         },
         {
-            "name": "BAIRRemove",
-            "on": true
+            name: "BAIRRemove",
+            on: true,
         },
         {
-            "name": "dynamicAdd",
-            "on": true
+            name: "dynamicAdd",
+            on: true,
         },
         {
-            "name": "dynamicRemoveByName",
-            "on": true
+            name: "dynamicRemoveByName",
+            on: true,
         },
         {
-            "name": "dynamicRemoveByUid",
-            "on": true
+            name: "dynamicRemoveByUid",
+            on: true,
         },
         {
-            "name": "game24p",
-            "on": true
+            name: "game24p",
+            on: true,
         },
         {
-            "name": "help",
-            "on": true
+            name: "help",
+            on: true,
         },
         {
-            "name": "kexuefmAdd",
-            "on": true
+            name: "kexuefmAdd",
+            on: true,
         },
         {
-            "name": "kexuefmRemove",
-            "on": true
+            name: "kexuefmRemove",
+            on: true,
         },
         {
-            "name": "liveAdd",
-            "on": true
+            name: "liveAdd",
+            on: true,
         },
         {
-            "name": "liveRemoveByName",
-            "on": true
+            name: "liveRemoveByName",
+            on: true,
         },
         {
-            "name": "liveRemoveByUid",
-            "on": true
+            name: "liveRemoveByUid",
+            on: true,
         },
         {
-            "name": "setu",
-            "on": true
+            name: "setu",
+            on: true,
         },
         {
-            "name": "videoAdd",
-            "on": true
+            name: "videoAdd",
+            on: true,
         },
         {
-            "name": "videoRemoveByName",
-            "on": true
+            name: "videoRemoveByName",
+            on: true,
         },
         {
-            "name": "videoRemoveByUid",
-            "on": true
+            name: "videoRemoveByUid",
+            on: true,
         },
         {
-            "name": "addJuejinSubscribe",
-            "on": true
+            name: "addJuejinSubscribe",
+            on: true,
         },
         {
-            "name": "removeJuejinSubscribe",
-            "on": true
+            name: "removeJuejinSubscribe",
+            on: true,
         },
         {
-            "name": "addGithubSubscribe",
-            "on": true
+            name: "addGithubSubscribe",
+            on: true,
         },
         {
-            "name": "removeGithubSubscribe",
-            "on": true
+            name: "removeGithubSubscribe",
+            on: true,
         },
         {
-            "name": "askForRa3Data",
-            "on": true
+            name: "askForRa3Data",
+            on: false,
         },
         {
-            "name": "randomRa3Joke",
-            "on": true
+            name: "randomRa3Joke",
+            on: false,
         },
         {
-            "name": "randomMoTalk",
-            "on": true
-        }
-    ]
-}
+            name: "randomMoTalk",
+            on: false,
+        },
+    ],
+};

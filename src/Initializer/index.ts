7554eb46434036ac3f21b5c4d48fe32ef77cb5df
newive <738767136@qq.com>
2023-04-02 16:44:21 +0800

    删除冗余代码

--- src/Initializer/index.ts
+++ src/Initializer/index.ts
@@ -2,13 +2,11 @@ import dbHandler from "../DBManager";
 import pluginLoader from "../PluginLoader";
 import qqCommand from "../QQCommand";
 import qq from "../QQMessage";
-import Checker from "../Checker";
 import configHandler from "../ConfigHandler";
 
 export async function initializer(): Promise<void> {
     let res = await configHandler.readGlobalConfig();
     if (res) {
-        await Checker.checkComplicity();
         // wsc连接与数据库初始化为其他服务的前驱
         await dbHandler.init();
         await pluginLoader.loadPlugins();
--- src/QQCommand/index.ts
+++ src/QQCommand/index.ts
@@ -5,8 +5,6 @@ import lodash from "lodash";
 import { PluginLoaderType } from "../PluginLoader/type";
 import {readFile, writeFile} from "../Util/fileHandler";
 import path from "path";
-import { RandomPicType } from "../Plugins/RandomPic/type";
-import RandomPic from "../Plugins/RandomPic";
 import {
     CommandConfigType,
     latestTemplateType,
@@ -37,12 +35,10 @@ function* iteConfig(configs: Array<PluginLoaderType.PluginConfig> | Array<templa
 
 export class CommandDispatcher {
     private commands: Array<CmdType.Cmd>;
-    public randomImgConf: Array<RandomPicType.RandomPicConf>;
     private tasks: Array<CmdType.Task>;
 
     constructor() {
         this.commands = [];
-        this.randomImgConf = [];
         this.tasks = [];
     }
 
@@ -156,112 +152,7 @@ export class CommandDispatcher {
         this.tasks.push(t);
     }
 
-    // eslint-disable-next-line @typescript-eslint/explicit-module-boundary-types
-    public async loadRandomPicCommand() {
-        let confS;
-        let conf: Array<RandomPicType.RandomPicConf>;
-        try {
-            confS = (
-                await readFile(path.resolve("randomPicCmdTemplate.config.json"))
-            ).data;
-        } catch (e) {
-            log.warn(e);
-            log.warn("读取随机图片配置失败");
-            return;
-        }
-        try {
-            conf = JSON.parse(
-                confS
-            ) as unknown as Array<RandomPicType.RandomPicConf>;
-        } catch (e) {
-            log.warn(e);
-            log.warn("无法解析JSON文件");
-            return;
-        }
-        for (const c of conf) {
-            if (this.validateRandomPicConf(c)) {
-                this.randomImgConf.push(c);
-                await RandomPic.initTemplateCmd(c.tableName, c.dirName);
-                let cmd;
-                if (!c.allowSpecial) {
-                    cmd = RandomPic.genRandomPicCmd(
-                        c.cmdPattern,
-                        c.cmdPattern,
-                        c.allowSpecial,
-                        c.tableName,
-                        c.dirName,
-                        c.messageTemplate
-                    );
-                } else {
-                    cmd = RandomPic.genRandomPicCmd(
-                        c.cmdPattern,
-                        c.cmdPattern,
-                        c.allowSpecial,
-                        c.tableName,
-                        c.dirName,
-                        c.messageTemplate,
-                        c.special,
-                        c.specialPicPath
-                    );
-                }
-                log.info(`加载命令: ${cmd.cmdName}`);
-                this.commands.push(cmd);
-                if (c.allowUpload) {
-                    const uploadCmd = RandomPic.genUploadPicCmd(
-                        c.uploadCmdPattern as string,
-                        c.uploadCmdPattern as string,
-                        c.tableName,
-                        c.dirName,
-                        c.uploadCmdAuthID as Array<number>
-                    );
-                    this.commands.push(uploadCmd);
-                    const newestCmd = RandomPic.genNewestCmd(
-                        c.newestCmdPattern as string,
-                        c.newestCmdPattern as string,
-                        c.tableName,
-                        c.dirName,
-                        c.messageTemplate
-                    );
-                    this.commands.push(newestCmd);
-                }
-            } else {
-                log.warn("非法的命令配置");
-            }
-        }
-    }
-
-    // eslint-disable-next-line @typescript-eslint/explicit-module-boundary-types
-    public validateRandomPicConf(c: RandomPicType.RandomPicConf) {
-        const check = (c: RandomPicType.RandomPicConf) =>
-            Object.prototype.hasOwnProperty.call(c, "cmdPattern") &&
-            Object.prototype.hasOwnProperty.call(c, "dirName") &&
-            Object.prototype.hasOwnProperty.call(c, "tableName") &&
-            Object.prototype.hasOwnProperty.call(c, "allowUpload") &&
-            Object.prototype.hasOwnProperty.call(c, "allowSpecial") &&
-            Object.prototype.hasOwnProperty.call(c, "messageTemplate");
-        const checkUpload = (c: RandomPicType.RandomPicConf) =>
-            Object.prototype.hasOwnProperty.call(c, "newestCmdPattern") &&
-            Object.prototype.hasOwnProperty.call(c, "uploadCmdPattern") &&
-            Object.prototype.hasOwnProperty.call(c, "uploadCmdAuthID") &&
-            Array.isArray(c.uploadCmdAuthID);
-        const checkSpecial = (c: RandomPicType.RandomPicConf) =>
-            Object.prototype.hasOwnProperty.call(c, "special") && Object.prototype.hasOwnProperty.call(c, "specialPicPath");
-        if (check(c)) {
-            if (!c.allowUpload && !c.allowSpecial) {
-                return true;
-            } else if (c.allowUpload && !c.allowSpecial) {
-                return checkUpload(c);
-            } else if (!c.allowUpload && c.allowSpecial) {
-                return checkSpecial(c);
-            } else {
-                return checkUpload(c) && checkSpecial(c);
-            }
-        }
-        return false;
-    }
-
-    // eslint-disable-next-line @typescript-eslint/explicit-module-boundary-types
-    public async loadTemplateCommand() {
+    public async loadTemplateCommand(): Promise<void>  {
         log.info("开始注册模板命令...");
         // await this.loadRandomPicCommand();
         await this.loadTemplateMessageCommand();
